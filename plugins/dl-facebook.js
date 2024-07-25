6667cafb4cd112eace8c8ab4dbc8747594b1925e
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-07-25 10:14:38 +0500

    💦ᴘʀɪɴᴄᴇ-ᴍᴅ💦

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,26 +1,22 @@
 
-import fg from 'api-dylux' 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
- if (!args[0] && m.quoted && m.quoted.text) {
-  args[0] = m.quoted.text;
-}
-if (!args[0] && !m.quoted) throw `✳️ Provide a Facebook link first \n📌 Example:\n*${usedPrefix + command}* https://www.facebook.com/...`
-    m.react(rwait)
-   try {
-    let result = await fg.fbdl(args[0]);
-    let tex = `
-┌─⊷ *⚪PRINCE FBDL⚪*
-▢ *Title:* ${result.title}
-└───────────`;
-    conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
-    m.react(done);
+import fg from 'api-dylux';
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  
+if (!args[0]) throw `✳️ ${mssg.noLink('Facebook')}\n\n📌 ${mssg.example} :\n*${usedPrefix + command}* past your fb video url`
+  m.react(rwait)
+
+  try {
+    let res = await fetch(global.API('fgmods', '/api/downloader/fbdl', { url: args[0] }, 'apikey'))
+    let data = await res.json()
+    
+    conn.sendFile(m.chat, data.result.HD, 'fb.mp4', `✅ ${mssg.result}`, m)
+    m.react(done)
   } catch (error) {
- 	m.reply('❎Enter a proper facebook link ')
- 	} 
+    m.reply(mssg.error)
+  }
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
-handler.tags = ['downloader']
+handler.tags = ['dl']
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i
-handler.diamond = false
 
 export default handler
