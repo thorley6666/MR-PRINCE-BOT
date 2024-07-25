cede088f747c898cf72d1a69504b4b2719e352b8
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-07-25 12:21:38 +0500

    💦ᴘʀɪɴᴄᴇ-ᴍᴅ💦

---
+++ gemini.js
@@ -0,0 +1,23 @@
+
+import fetch from 'node-fetch';
+let handler = async (m, { conn, text }) => {
+	
+if (!text && !(m.quoted && m.quoted.text)) {
+    m.reply(`✳️Please provide some text or quote a message to get a response.`);
+  
+m.react('💬')
+
+    try {
+		let gpt = await fetch(global.API('fgmods', '/api/info/gemini', { text }, 'apikey'));
+        let res = await gpt.json()
+        await m.reply(res.result)
+	} catch {
+		m.reply(`❎ Error: Try Later`);
+	}
+
+}
+handler.help = ['gemini <text>']; 
+handler.tags = ['tools'];
+handler.command = ['gemini'];
+
+export default handler;
