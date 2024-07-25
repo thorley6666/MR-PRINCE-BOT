ab803951937fbc4d71c59faec2dc1bd43d2bd26e
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-07-25 14:22:56 +0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,30 +1,38 @@
+import yts from 'yt-search';
 
-import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-	
-  if (!text) throw `✳️ Example: *${usedPrefix + command}* Mujy Rang de Naat`
-	let res = await yts(text)
-	let vid = res.videos[0]
-	if (!vid) throw `✳️ Video/Audio No found`
-	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
-	//const url = 'https://www.youtube.com/watch?v=' + videoId
-	m.react('🎧') 
+  if (!text) throw `✳️ Example: *${usedPrefix + command}* Mujy Rang de Naat`;
+
+  let res = await yts(text);
+
+  // Check if res.videos is not null and has at least one element
+  if (!res.videos || res.videos.length === 0) {
+    throw `✳️ Video/Audio No found`;
+  }
+
+  let vid = res.videos[0];
+  let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid;
+
+  m.react('🎧');
+
   let play = `
-	 𓆩 𓅓 𓆪 *PRINCE STUDIO*
+    𓆩 𓅓 𓆪 *PRINCE STUDIO*
 ┌──────────────
 ┃ 📌 *TITLE:* ${vid.title}
 ┃ 📆 *UPLOADED:* ${vid.ago}
 ┃ ⌚ *DURATION:* ${vid.timestamp}
 ┃ 👀 *VIEWS:* ${vid.views.toLocaleString()}
-└──────────────`
- await conn.sendButton2(m.chat, play, thumbnail, [
+└──────────────`;
+
+  await conn.sendButton2(m.chat, play, thumbnail, [
     ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
     ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
-  ], [['Channel', `${princegp}`]], m)
-}
-handler.help = ['play']
-handler.tags = ['dl']
-handler.command = ['play', 'playvid']
-handler.disabled = false
+  ], null, [['Channel', `${princegp}`]], m);
+};
 
-export default handler
+handler.help = ['play'];
+handler.tags = ['dl'];
+handler.command = ['play', 'playvid'];
+handler.disabled = false;
+
+export default handler;
