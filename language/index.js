82cc1135b02a1c21cc1534b2c329bc16748a8897
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-07-31 23:03:41 +0500

    𝑷-𝑴𝑫

--- language/index.js
+++ language/index.js
@@ -4,5 +4,6 @@ import es from './es.js'
 import pt from './pt.js' 
 import id from './id.js'
 import ar from './ar.js'
+import ur from './ur.js'
 
-export {en, es, pt, id, ar}
+export {en, es, pt, id, ar, ur}
